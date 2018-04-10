a78eb904a7089ea0c9f88ffcee41957cee84220e
alexbaranov43 <alexbaranov43@gmail.com>
2018-04-10 00:50:14 -0400

    Some Dry Code

--- liri.js
+++ liri.js
@@ -52,29 +52,13 @@ function spotifySong(search) {
         })
     }
     else {
-        client.search({ type: 'track', query: "The Sign Ace of Base", limit: '5' }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            console.log("----------------------");
-            for (i = 0; i < data.tracks.items.length; i++) {
-                var artists = [];
-                console.log("*-------------------------------------*")
-                for (i2 = 0; i2 < data.tracks.items[i].artists.length; i2++) {
-                    artists.push(data.tracks.items[i].artists[i2].name)
-                }
-                console.log("Artists: " + artists.join(", "))
-                console.log("Title: " + data.tracks.items[i].name)
-                console.log("Album: " + data.tracks.items[i].album.name)
-                console.log("Preview Link: " + data.tracks.items[i].preview_url)
-            }
-        })
-
+        spotifySong("The Sign Ace of Base")
     }
+
 }
 
 //movie search function
-function movieSearch() {
+function movieSearch(search) {
     if (search) {
         request("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
             if (!error && response.statusCode === 200) {
@@ -89,45 +73,39 @@ function movieSearch() {
             }
         });
     } else {
-        request("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(JSON.parse(body).Title);
-                console.log("Release Year :" + JSON.parse(body).Year);
-                console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Produced In: " + JSON.parse(body).Country);
-                console.log("Languge[s] :" + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors)
-            }
-        })
+        movieSearch("Mr. Nobody")
     }
+
 }
 
 //twitter function
 function twitterTweets() {
     var client = twitter(keys.twitter)
     client.get('statuses/user_timeline', { screen_name: 'alexbaranov43', count: 20 }, function (error, tweets, response) {
-        if (error) throw error;
-        for (i = 0; i < tweets.length; i++) {
-            console.log("*---------------------*")
-            console.log(tweets[i].created_at)
-            console.log(tweets[i].text);
+        if (error) {
+
+            return console.log(error);
+        }
+        else {
+            for (i = 0; i < tweets.length; i++) {
+                console.log("*---------------------*")
+                console.log(tweets[i].created_at)
+                console.log(tweets[i].text);
+            }
         }
     });
 
 }
-
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
             return console.log(err)
         }
         var dataArr = data.split(",");
-        console.log(dataArr);
+
         for (i = 0; i < dataArr.length; i += 2) {
             if (dataArr[i] === "spotify-this-song") {
-                spotifySong(dataArr[i++]);
+                spotifySong(dataArr[1])
             }
         }
     })
