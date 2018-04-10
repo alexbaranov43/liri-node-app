471ee20f6843d5c087234d8d8b421cb79492182b
alexbaranov43 <alexbaranov43@gmail.com>
2018-04-09 22:06:03 -0400

    working and done

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var keys = require("./key.js")
 
-
+var fs = require("fs")
 var twitter = require("twitter");
 var dotenv = require("dotenv");
 var request = require("request");
@@ -10,14 +10,34 @@ var spotify = require("node-spotify-api");
 var command = process.argv[2];
 var search = process.argv[3];
 
-
+//command prompt commands
 if (command === "spotify-this-song") {
-    var client = new spotify(keys.spotify);
+    spotifySong(search);
+}
+else if (command === "movie-this") {
+    movieSearch()
+}
+else if (command === "do-what-it-says") {
+    doWhatItSays()
+
+}
+else if (command === "my-tweets") {
+    twitterTweets();
+}
+
+
+
+////functions 
+
+//spotify function
+function spotifySong(search) {
+    var client = new spotify(keys.spotify)
     if (search) {
-        client.search({ type: 'track', query: search, limit: 5 }, function (err, data) {
+        client.search({ type: 'track', query: search, limit: '5' }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
+            console.log("----------------------");
             for (i = 0; i < data.tracks.items.length; i++) {
                 var artists = [];
                 console.log("*-------------------------------------*")
@@ -32,24 +52,29 @@ if (command === "spotify-this-song") {
         })
     }
     else {
-        {
-            client.search({ type: 'track', query: "The Sign Ace of Base ", limit: 1 }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
+        client.search({ type: 'track', query: "The Sign Ace of Base", limit: '5' }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log("----------------------");
+            for (i = 0; i < data.tracks.items.length; i++) {
+                var artists = [];
+                console.log("*-------------------------------------*")
+                for (i2 = 0; i2 < data.tracks.items[i].artists.length; i2++) {
+                    artists.push(data.tracks.items[i].artists[i2].name)
                 }
-                for (i = 0; i < 5; i++) {
-                    console.log("*-------------------------------------*")
-                    console.log("Artists: " + data.tracks.items[i].artists[0].name)
-                    console.log("Title: " + data.tracks.items[i].name)
-                    console.log("Album: " + data.tracks.items[i].album.name)
-                    console.log("Preview Link: " + data.tracks.items[i].preview_url)
-                }
-            })
-        }
+                console.log("Artists: " + artists.join(", "))
+                console.log("Title: " + data.tracks.items[i].name)
+                console.log("Album: " + data.tracks.items[i].album.name)
+                console.log("Preview Link: " + data.tracks.items[i].preview_url)
+            }
+        })
 
     }
 }
-else if (command === "movie-this") {
+
+//movie search function
+function movieSearch() {
     if (search) {
         request("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
             if (!error && response.statusCode === 200) {
@@ -78,10 +103,9 @@ else if (command === "movie-this") {
         })
     }
 }
-else if (command === "do-what-it-says") {
 
-}
-else if (command === "my-tweets") {
+//twitter function
+function twitterTweets() {
     var client = twitter(keys.twitter)
     client.get('statuses/user_timeline', { screen_name: 'alexbaranov43', count: 20 }, function (error, tweets, response) {
         if (error) throw error;
@@ -89,9 +113,22 @@ else if (command === "my-tweets") {
             console.log("*---------------------*")
             console.log(tweets[i].created_at)
             console.log(tweets[i].text);
-
         }
     });
 
+}
 
-};
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err)
+        }
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        for (i = 0; i < dataArr.length; i += 2) {
+            if (dataArr[i] === "spotify-this-song") {
+                spotifySong(dataArr[i++]);
+            }
+        }
+    })
+}
